d86712621f5efb98e186ac75ea21f570a83d01df
Italo Carvalho <italo.998.carvalho@gmail.com>
2018-03-28 10:51:11 -0300

    Jogada especial roque pequeno e roque grande

--- xadrez-console1/xadrez/Rei.cs
+++ xadrez-console1/xadrez/Rei.cs
@@ -9,9 +9,11 @@ namespace xadrez
 {
     class Rei : Peca
     {
+        private PartidaDeXadrez partida;
 
-        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        public Rei(Tabuleiro tab, Cor cor, PartidaDeXadrez partida) : base(tab, cor)
         {
+            this.partida = partida;
         }//construtor
 
         public override string ToString()
@@ -24,6 +26,12 @@ namespace xadrez
             Peca p = tab.peca(pos);
             return p == null || p.cor != cor;
         }//podeMover
+        
+        private bool testeTorreParaRoque(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p != null && p is Torre && p.cor == cor && p.qteMovimentos == 0;
+        }//testeTorreParaRoque
 
         public override bool[,] movimentosPossiveis()
         {
@@ -87,7 +95,36 @@ namespace xadrez
             {
                 mat[pos.linha, pos.coluna] = true;
             }
-            
+
+            //#jogadaEspecial roque
+            if(qteMovimentos == 0 && !partida.xeque)
+            {
+                //roque pequeno
+                Posicao posT1 = new Posicao(posicao.linha, posicao.coluna + 3);
+                if(testeTorreParaRoque(posT1))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna + 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna + 2);
+                    if(tab.peca(p1) == null && tab.peca(p2) == null)
+                    {
+                        mat[posicao.linha, posicao.coluna + 2] = true;
+                    }
+                }
+
+                //roque grande
+                Posicao posT2 = new Posicao(posicao.linha, posicao.coluna - 4);
+                if (testeTorreParaRoque(posT2))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna - 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna - 2);
+                    Posicao p3 = new Posicao(posicao.linha, posicao.coluna - 3);
+                    if (tab.peca(p1) == null && tab.peca(p2) == null && tab.peca(p3) == null)
+                    {
+                        mat[posicao.linha, posicao.coluna - 2] = true;
+                    }
+                }
+            }
+
             return mat;
         }//movimentosPossiveis
 
